1a4f9f5d99c2520fc45d2ac97f2000fdf70061f0
Ashley Ordoñez <mcordonez3@up.edu.ph>
2024-01-13 00:53:05 +0800

    Update Room Repository

--- hbs-api/src/main/java/mp/hbsapi/repository/RoomRepository.java
+++ hbs-api/src/main/java/mp/hbsapi/repository/RoomRepository.java
@@ -15,7 +15,7 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
     @Query(value = "SELECT COUNT(*) FROM ROOM WHERE ROOM_TYPE=:type", nativeQuery = true)
     int getRoomCountByType(@Param("type") char type);
 
-    @Query(value = "SELECT ROOM_NUMBER FROM ROOM WHERE IS_BOOKED = 0", nativeQuery = true)
+    @Query(value = "SELECT ROOM_NUMBER FROM ROOM WHERE ROOM_TYPE = ?1 AND IS_BOOKED = 0", nativeQuery = true)
     List<Integer> getAvailableRoomsByType(char type);
 
     @Query(value = "UPDATE ROOM SET IS_BOOKED = ?1 WHERE ROOM_NUMBER = ?2", nativeQuery = true)
